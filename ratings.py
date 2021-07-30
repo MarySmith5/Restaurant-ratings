ca3aa4044ae34c6a58ae0330fbb3b99eb67fe255
Mary Smith <mary.sunshine4@gmail.com>
2021-07-30 13:25:41 +0000

    Worked out some bugs.

--- ratings.py
+++ ratings.py
@@ -1,53 +1,117 @@
 """Restaurant rating lister."""
 
 
-# put your code here
+def choose_option():
+    """Give user options and return the chosen option."""
 
-ratings_info = open('scores.txt')
-#Import needed info
+    while True:
 
-restaurant_ratings = {}
+        options = ['S', 'A', 'U', 'Q']
+        print( "Would you like to:\n"
+                "S: See restaurants/ratings\n"
+                "A: Add a restautant/rating\n"
+                "U: Update a rating\n"
+                "Q: quit")
 
-#User option to add restaurant and rating 
-while True:
+        option = input('> ').upper()
 
-    choice = input("Would you like to make an entry? Y or N: ").upper()
+        if option not in options:
+            print('Enter S, A, U, or Q')
 
-    if choice.startswith('Y'):
-
-        rest_name = input("Enter the restaurant name: ").title()
-
-        while True:
-
-            rating = int(input("Enter a rating between 1 and 5: "))
-            if rating < 1 or rating > 5:
-                print("Invalid.")
-
-            else:
-                break
+        else:
+            return option
 
 
-        restaurant_ratings[rest_name] = rating
+def add_restaurant(restaurants_file):
+    """User add restaurant and rating """
+        
+    rest_name = input("Enter the restaurant name: ").title()
 
-    else:
-        break
+    while True:
 
-#Process info into dictionary
-for line in ratings_info:
-    line = line.rstrip()
-    place_and_rating = line.split(':')
+        rating = int(input("Enter a rating between 1 and 5: "))
+        if rating < 1 or rating > 5:
+            print("Invalid.")
 
-    restaurant_ratings[place_and_rating[0]] = place_and_rating[1]
+        else:
+            break
+            
+    ratings_info = open(restaurants_file, 'a')
+    ratings_info.write(f'{rest_name}:{rating}\n')
+    ratings_info.close()
 
-#Sort the dictionary
-sort_me = restaurant_ratings.items()
-rest_rate_sorted = sorted(sort_me)
-sorted_restaurant_ratings = dict(rest_rate_sorted)
+    print("Restaurant added.")           
+     
 
-#Print dictionary in readable sorted lines.
-for key in sorted_restaurant_ratings:
-    print(f"{key} is rated at {sorted_restaurant_ratings[key]}.")
+def make_dict(restaurants_file):
+    """Process info into dictionary."""
 
-ratings_info.close()
-#print(sorted_restaurant_ratings)
+    ratings_info = open(restaurants_file)
+    restaurant_ratings = {}
+
+    for line in ratings_info:
+        line = line.rstrip()
+        place_and_rating = line.split(':')
+
+        restaurant_ratings[place_and_rating[0]] = place_and_rating[1]
+
+    #Sort the dictionary
+
+    sort_me = restaurant_ratings.items()
+    rest_rate_sorted = sorted(sort_me)
+    
+    sorted_restaurant_ratings = dict(rest_rate_sorted)
+
+    ratings_info.close()
+
+    return sorted_restaurant_ratings
+
+
+def see_restaurants(sorted_dict):
+    """Print dictionary in readable sorted lines."""
+
+    for key in sorted_dict:
+        print(f"{key} is rated at {sorted_dict[key]}.")
+
+
+#def update_rating()
+
+
+def run_main(restaurants_file):
+    """Run restaurant ratings program."""
+
+    print(" Welcome to the restaurant rater!")
+    print()
+
+    while True:
+
+        sorted_restaurants = make_dict(restaurants_file)
+
+        do_this = choose_option()
+
+        if do_this == "S":
+
+            see_restaurants(sorted_restaurants)
+            print()
+
+        elif do_this == "A":
+
+            add_restaurant(restaurants_file)
+            
+            print()
+
+        elif do_this == "Q":
+
+            print("Bye")
+            break
+
+
+run_main('scores.txt')
+
+            
+
+            
+
+
+
 
